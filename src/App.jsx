7b9957fce6ce9796166865e5effa9cc6937f385e
Leandro-Pinho <leandroessencia@gmail.com>
2023-08-09 10:39:59 -0300

    criando o html

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+// components
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
 
 import './App.css'
 
 function App() {
-  
+
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>etapas</p>
+        <form>
+          <div className="input-container"></div>
+          <div className="actions">
+            <button type='button'>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button type='submit'>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
