f700fc2794392f13cf8373e4909e7d7d5ddceb72
Leandro-Pinho <leandroessencia@gmail.com>
2023-08-11 10:58:21 -0300

    criando a review form e estilizando

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,8 +1,35 @@
+import { BsFillEmojiHeartEyesFill, BsFillEmojiSmileFill, BsFillEmojiNeutralFill, BsFillEmojiFrownFill } from 'react-icons/bs'
+import './ReviewForm.css'
 
 const ReviewForm = () => {
     return (
-        <div>
-            Review
+        <div className="review-form">
+            <div className="form-control score-container">
+                <label className="radio-container">
+                    <input type="radio" value='unsatisfied' name="review" required />
+                    <BsFillEmojiFrownFill />
+                    <p>Insatisfeito</p>
+                </label>
+                <label className="radio-container">
+                    <input type="radio" value='neutral' name="review" required />
+                    <BsFillEmojiNeutralFill />
+                    <p>poderia ser melhor</p>
+                </label>
+                <label className="radio-container">
+                    <input type="radio" value='satisfied' name="review" required />
+                    <BsFillEmojiSmileFill />
+                    <p>Satisfeito</p>
+                </label>
+                <label className="radio-container">
+                    <input type="radio" value='very_satisfied' name="review" required />
+                    <BsFillEmojiHeartEyesFill />
+                    <p>Muito satisfeito</p>
+                </label>
+            </div>
+            <div className="form-control">
+                <label htmlFor="comment">Comentário:</label>
+                <textarea name="comment" id="comment" placeholder='Conte como foi a sua experiência com o produto...' required cols="30" rows="10"></textarea>
+            </div>
         </div>
     )
 }
